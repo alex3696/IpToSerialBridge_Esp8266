f4659433b98b109418780636cfb313c90bba384c
SAVPC\Sav <alex3696@yandex.ru>
2026-04-16 11:47:19 +0700

    REFR - remove Modbus checks

--- IpToSerialBridge_Esp8266/IpToSerialBridge_Esp8266.ino
+++ IpToSerialBridge_Esp8266/IpToSerialBridge_Esp8266.ino
@@ -2,7 +2,7 @@
 #include <WiFiUdp.h>
 // #include <WiFiServer.h>
 /*
- * Bridge UDP/TCP <-> serial.
+   Bridge UDP/TCP <-> serial.
 */
 //----------------------------------------------------------------------
 // output debugging information to UART1 TX
@@ -16,35 +16,51 @@
 #endif
 //----------------------------------------------------------------------
 // Serial config
-#define RS_Speed 2000000	// (921600) скорость соединения по Serial
-#define RS_pin 12			// выход для переключения направления передачи
-#define RS_Timeout 10		// таймаут Serial
-#define RS_StTimeout 500	// таймаут приема Serial
-#define RS_StTimeout23 2500 // таймаут приема Serial для команды 23
-#define Server_Timeout 2000 // таймаут сервера
+const uint32_t RS_Speed = 2000000;	      // (921600) скорость соединения по Serial
+const uint32_t RS_pin = 12;			          // выход для переключения направления передачи
+const uint32_t Server_Timeout = 2000;     // таймаут сервера
 #define BUF_MAX 256
-//----------------------------------------------------------------------
-#define bswap16(x) (((unsigned short)(x)) << 8 | ((unsigned short)(x)) >> 8)
+
+const size_t charTimeoutMicros = 20;    // pause indicating the end of a continuous packet, таймаут Serial
+uint8_t ipBuf[BUF_MAX];
+uint8_t serBuf[BUF_MAX];
+uint8_t serPos = 0;
+size_t serLastByteMicros = 0;
+
+const size_t channelTimeoutMillis = 2000; // Сброс источника через 2 сек тишины
+size_t lastActivityMillis = 0;     // Для сброса lastSource
+
+// откуда пришёл последний пакет
+enum Source
+{
+  NONE,
+  SOURCE_TCP,
+  SOURCE_UDP
+};
+Source lastSource = NONE;
+IPAddress udpRemoteIp;
+uint16_t udpRemotePort;
+
 //----------------------------------------------------------------------
 // default WiFi client config
 // String Statuses[] =  { "WL_IDLE_STATUS=0", "WL_NO_SSID_AVAIL=1", "WL_SCAN_COMPLETED=2", "WL_CONNECTED=3", "WL_CONNECT_FAILED=4", "WL_CONNECTION_LOST=5", "WL_DISCONNECTED=6"};
-//----------------------------------------------------------------------
 //  defaul WiFi AP config
 WiFiMode wifi_mode = WiFiMode::WIFI_STA;
 String ssidAPbase = "bs3";
+String ssidAP = "";
 String passwordAP = "spsksiam";
 IPAddress ip_static(10, 10, 10, 10);
 IPAddress ip_gateway(10, 10, 10, 1);
 IPAddress ip_subnet(255, 255, 255, 0);
 IPAddress ip_dns(10, 10, 10, 1);
 //----------------------------------------------------------------------
+const int Port = 502;
 WiFiUDP udp;
-WiFiServer server(502);
+WiFiServer server(Port);
 WiFiClient tcpClient;
-uint8_t wifiCondition = 0;
-uint8_t buf[BUF_MAX];
-String ssidAP = "";
-
+int wifiCondition = 0;
+const size_t connectCheckPeriod = 300;
+size_t connectCheckLast = 0;
 //-----------------------------------------------------------------------------
 // Quick pin output
 void IRAM_ATTR WritePin(uint8_t pin, uint8_t val)
@@ -66,8 +82,8 @@ void IRAM_ATTR WritePin(uint8_t pin, uint8_t val)
 }
 //----------------------------------------------------------------------
 // ESP-12E module's onboard LED, used as status indicator
-#define BUILTIN_LED 2 
-uint8_t LedState = 0;
+#define BUILTIN_LED 2
+int LedState = 0;
 
 void BUILTIN_LED_OFF()
 {
@@ -90,68 +106,41 @@ void BUILTIN_LED_TOOGLE()
   else
     BUILTIN_LED_ON();
 }
-unsigned long gPreviosCycleQty = 0;
-void idle_blink();
 
-//----------------------------------------------------------------------
-//----- Таблица для вычисления CRC: -----
-const uint16_t Crc16Table[] PROGMEM = {
-  0x0000, 0xC0C1, 0xC181, 0x0140, 0xC301, 0x03C0, 0x0280, 0xC241,
-  0xC601, 0x06C0, 0x0780, 0xC741, 0x0500, 0xC5C1, 0xC481, 0x0440,
-  0xCC01, 0x0CC0, 0x0D80, 0xCD41, 0x0F00, 0xCFC1, 0xCE81, 0x0E40,
-  0x0A00, 0xCAC1, 0xCB81, 0x0B40, 0xC901, 0x09C0, 0x0880, 0xC841,
-  0xD801, 0x18C0, 0x1980, 0xD941, 0x1B00, 0xDBC1, 0xDA81, 0x1A40,
-  0x1E00, 0xDEC1, 0xDF81, 0x1F40, 0xDD01, 0x1DC0, 0x1C80, 0xDC41,
-  0x1400, 0xD4C1, 0xD581, 0x1540, 0xD701, 0x17C0, 0x1680, 0xD641,
-  0xD201, 0x12C0, 0x1380, 0xD341, 0x1100, 0xD1C1, 0xD081, 0x1040,
-  0xF001, 0x30C0, 0x3180, 0xF141, 0x3300, 0xF3C1, 0xF281, 0x3240,
-  0x3600, 0xF6C1, 0xF781, 0x3740, 0xF501, 0x35C0, 0x3480, 0xF441,
-  0x3C00, 0xFCC1, 0xFD81, 0x3D40, 0xFF01, 0x3FC0, 0x3E80, 0xFE41,
-  0xFA01, 0x3AC0, 0x3B80, 0xFB41, 0x3900, 0xF9C1, 0xF881, 0x3840,
-  0x2800, 0xE8C1, 0xE981, 0x2940, 0xEB01, 0x2BC0, 0x2A80, 0xEA41,
-  0xEE01, 0x2EC0, 0x2F80, 0xEF41, 0x2D00, 0xEDC1, 0xEC81, 0x2C40,
-  0xE401, 0x24C0, 0x2580, 0xE541, 0x2700, 0xE7C1, 0xE681, 0x2640,
-  0x2200, 0xE2C1, 0xE381, 0x2340, 0xE101, 0x21C0, 0x2080, 0xE041,
-  0xA001, 0x60C0, 0x6180, 0xA141, 0x6300, 0xA3C1, 0xA281, 0x6240,
-  0x6600, 0xA6C1, 0xA781, 0x6740, 0xA501, 0x65C0, 0x6480, 0xA441,
-  0x6C00, 0xACC1, 0xAD81, 0x6D40, 0xAF01, 0x6FC0, 0x6E80, 0xAE41,
-  0xAA01, 0x6AC0, 0x6B80, 0xAB41, 0x6900, 0xA9C1, 0xA881, 0x6840,
-  0x7800, 0xB8C1, 0xB981, 0x7940, 0xBB01, 0x7BC0, 0x7A80, 0xBA41,
-  0xBE01, 0x7EC0, 0x7F80, 0xBF41, 0x7D00, 0xBDC1, 0xBC81, 0x7C40,
-  0xB401, 0x74C0, 0x7580, 0xB541, 0x7700, 0xB7C1, 0xB681, 0x7640,
-  0x7200, 0xB2C1, 0xB381, 0x7340, 0xB101, 0x71C0, 0x7080, 0xB041,
-  0x5000, 0x90C1, 0x9181, 0x5140, 0x9301, 0x53C0, 0x5280, 0x9241,
-  0x9601, 0x56C0, 0x5780, 0x9741, 0x5500, 0x95C1, 0x9481, 0x5440,
-  0x9C01, 0x5CC0, 0x5D80, 0x9D41, 0x5F00, 0x9FC1, 0x9E81, 0x5E40,
-  0x5A00, 0x9AC1, 0x9B81, 0x5B40, 0x9901, 0x59C0, 0x5880, 0x9841,
-  0x8801, 0x48C0, 0x4980, 0x8941, 0x4B00, 0x8BC1, 0x8A81, 0x4A40,
-  0x4E00, 0x8EC1, 0x8F81, 0x4F40, 0x8D01, 0x4DC0, 0x4C80, 0x8C41,
-  0x4400, 0x84C1, 0x8581, 0x4540, 0x8701, 0x47C0, 0x4680, 0x8641,
-  0x8201, 0x42C0, 0x4380, 0x8341, 0x4100, 0x81C1, 0x8081, 0x4040
-};
-//----------------------------------------------------------------------
-uint16_t CRC_modbus;
-void CRC_16(uint8_t *b, uint16_t count)
+//-----------------------------------------------------------------------------
+unsigned long blinkPrevios = 0;
+const unsigned long blinkPeriod = 1500;
+void StatusBlink()
 {
-  CRC_modbus = 0xffff;
-  for (uint16_t i = 0; i < count; i++)
+  switch (lastSource)
   {
-    uint8_t ptr = (CRC_modbus & 0x00ff) ^ ((uint8_t)b[i] & 0x00ff);
-    CRC_modbus = pgm_read_word_near(Crc16Table + ptr) ^ (CRC_modbus >> 8);
+    default: break;
+    case SOURCE_TCP:
+    case SOURCE_UDP:
+      BUILTIN_LED_ON(); return;
+      //blinkPeriod = 200; break;
+  }
+  if (millis() - blinkPrevios > blinkPeriod)
+  {
+    BUILTIN_LED_TOOGLE();
+    blinkPrevios = millis();
   }
 }
 //-----------------------------------------------------------------------------
 void ClearSerial()
 {
   int clrRetry = BUF_MAX;
-  int avail = 0;
+  size_t avail = 0;
   while (clrRetry-- && (avail = Serial.available()))
   {
     DPPRINT(F("serial reset the incoming byte = ")); DPPRINTLN(avail, DEC);
-    int toRead = (avail >= BUF_MAX) ? BUF_MAX : avail;
-    Serial.readBytes(buf, toRead);
-    buf[toRead - 1] = 0;
-    DPPRINTLN((char*)buf);
+    size_t toRead = (avail > sizeof(ipBuf)) ? sizeof(ipBuf) : avail;
+    if (toRead)
+    {
+      Serial.readBytes(serBuf, toRead);
+      serBuf[toRead - 1] = 0;
+      DPPRINTLN((char*)buf);
+    }
   }
 }
 //----------------------------------------------------------------------
@@ -172,12 +161,12 @@ void setup()
 
   // SERIAL SETUP
   Serial.setRxBufferSize(2048);
-  Serial.setTimeout(0);//RS_Timeout
+  Serial.setTimeout(0);// не будем ожидать байты
   Serial.begin(RS_Speed, SERIAL_8N1);
   DPPRINTLN("setup serial port speed - OK");
 
-  // WIFI SETUP
-  // PIN_14
+  // SETUP WIFI MODE, VIA PIN_14
+  // 1=WIFI_STA; 0=WIFI_AP
   pinMode(14, INPUT_PULLUP); //
   // digitalWrite(14, HIGH);
 
@@ -240,7 +229,6 @@ void setup()
   Serial.pins(15, 13);
   pinMode(RS_pin, OUTPUT);
   digitalWrite(RS_pin, 0);
-
   ClearSerial();
 
   wifi_set_sleep_type(NONE_SLEEP_T);
@@ -250,7 +238,7 @@ void setup()
 //----------------------------------------------------------------------
 void StartIpServer()
 {
-  udp.begin(502);
+  udp.begin(Port);
   DPPRINTLN("UDP started");
   server.begin();
   server.setNoDelay(true);
@@ -273,7 +261,6 @@ void WifiConnectReset()
   DPPRINTLN("TCP stopped");
   WiFi.disconnect(true);
   WiFi.softAPdisconnect(true);
-
   switch (wifi_mode)
   {
     default:
@@ -292,10 +279,8 @@ void WifiConnectAP()
 {
   DPPRINTLN("Start WIFI_AP");
   WiFi.mode(WIFI_AP);
-
   WiFi.softAP(ssidAP.c_str(), passwordAP.c_str());
   WiFi.softAPConfig(ip_static, ip_gateway, ip_subnet);
-
   StartIpServer();
 }
 //----------------------------------------------------------------------
@@ -317,177 +302,12 @@ void WifiConnectSTA()
   }
   WifiConnectReset();
 }
-
-//----------------------------------------------------------------------
-int SerialBufCheck(int c)
-{
-  int packet_ok = 0, crc_ok = 0;
-
-  if (c >= 5) // Минимальная длина ответа 5 байт (exception)
-  {
-    if ((buf[1] & 0x80) == 0) // Если нормальный ответ (нет exception)
-    {
-      switch (buf[1])
-      {
-        case 3:
-        case 23:
-          if (c == buf[2] + 5)
-            packet_ok = 1;
-          break;
-
-        case 6:
-        case 16:
-          if (c == 8)
-            packet_ok = 1;
-          break;
-
-        default:
-          break;
-      }
-    }
-    else
-      packet_ok = 1;
-
-    if (packet_ok)
-    {
-      CRC_16(buf, c); // проверка контрольной суммы
-      if (CRC_modbus == 0)
-        crc_ok = 1;
-    }
-  }
-
-  return crc_ok;
-}
 //-----------------------------------------------------------------------------
-int ServerReceive(int avail)
-{
-  int packet_ok = 0, crc_ok = 0, c = 0;
-
-  if (avail >= 8)
-  {
-    c = tcpClient.readBytes(buf, avail); // минимальный пакет Modbus-RTU
-    if (c == avail)
-    {
-      switch (buf[1])
-      {
-        case 3:
-        case 6:
-          if (c == 8)
-            packet_ok = 1;
-          break;
-
-        case 16:
-          if (c == buf[6] + 7 + 2)
-            packet_ok = 1;
-          break;
-
-        case 23:
-          if (c >= 13 && c == buf[10] + 11 + 2)
-            packet_ok = 1;
-          break;
-
-        default:
-          break;
-      }
-
-      if (packet_ok)
-      {
-        CRC_16(buf, c); // проверка контрольной суммы
-        if (CRC_modbus == 0)
-          crc_ok = 1;
-      }
-    }
-  }
-  return crc_ok;
-}
-//-----------------------------------------------------------------------------
-void ModbusIo()
-{
-  int avail = tcpClient.available();
-  if (avail > BUF_MAX)
-    avail = BUF_MAX;
-  if (avail)
-  {
-    if (ServerReceive(avail))
-    {
-      int rxCnt = 0, bufCnt = 0, rxCorrect = 0;
-      char* bufPtr = (char*)buf;
-      unsigned long lastTick = millis();
-      unsigned long rsStTimeout = buf[1] == 23 ? RS_StTimeout23 : RS_StTimeout;
-      BUILTIN_LED_ON();
-      Serial.flush();
-      Serial.write(buf, avail);
-      DPPRINTLN("Begin data IO");
-      DPPRINT(F("20: TCP data avail = ")); DPPRINTLN(avail, DEC);
-      while (rxCorrect == 0 && ((millis() - lastTick) < rsStTimeout))
-      {
-        int serAvail = Serial.available();
-        if (serAvail > 0)
-        {
-          // 1. Рассчитываем, сколько байт реально можем принять
-          int toRead = (bufCnt + serAvail > BUF_MAX) ? BUF_MAX - bufCnt : serAvail;
-          if (0 < toRead)
-          {
-            DPPRINT(F("24: Serial data avail = ")); DPPRINTLN(toRead, DEC);
-            rxCnt = Serial.readBytes(bufPtr, toRead);
-            DPPRINT(F("25: condition = ")); DPPRINTLN(wifiCondition, DEC);
-            bufPtr += rxCnt;
-            bufCnt += rxCnt;
-            rxCorrect = SerialBufCheck(bufCnt);
-          }
-          // 4. Если буфер заполнился, а пакет так и не распознан — выходим
-          if (bufCnt >= BUF_MAX/* && !rxCorrect*/)
-          {
-            DPPRINTLN(F("Buffer overflow, no valid packet, flush serial"));
-            DPPRINT(F(" condition = ")); DPPRINTLN(wifiCondition, DEC);
-            Serial.flush();
-            break;
-          }
-        }//if (serAvail > 0)
-      }//while
-
-      BUILTIN_LED_OFF();
-      if (rxCorrect)
-      {
-        DPPRINTLN(F("valid packet, Send to TCP"));
-        tcpClient.write(buf, bufCnt); // отсылаем ответ ModbusTCP-клиенту
-      }
-      else
-      {
-        DPPRINT(F("no valid packet, bufCnt = ")); DPPRINTLN(bufCnt, DEC);
-        DPPRINTLN("flush serial");
-        Serial.flush();
-      }
-      gPreviosCycleQty = millis();
-      LedState = 0;
-      DPPRINT("End data IO");
-      DPPRINT(F(" condition = ")); DPPRINTLN(wifiCondition, DEC);
-    }
-    else
-      tcpClient.flush();
-  }//if (avail)
-}
-//-----------------------------------------------------------------------------
-const uint32_t charTimeoutMicros = 20;    // Пауза внутри пакета
-uint8_t ipBuf[BUF_MAX];
-uint8_t serBuf[BUF_MAX];
-uint8_t serPos = 0;
-unsigned long serLastByteMicros = 0;
-
-const uint32_t channelTimeoutMillis = 2000; // Сброс источника через 2 сек тишины
-unsigned long lastActivityMillis = 0; // Для сброса lastSource
-enum Source { NONE, SOURCE_TCP, SOURCE_UDP };
-Source lastSource = NONE;
-IPAddress udpRemoteIp;
-uint16_t udpRemotePort;
-
-
-
 void TransparentIo()
 {
   //BUILTIN_LED_ON();
-  uint32_t avail = 0;
-  uint32_t toRead = 0;
+  size_t avail = 0;
+  size_t toRead = 0;
 
   // ip to serial
   if (tcpClient && tcpClient.connected())
@@ -524,7 +344,7 @@ void TransparentIo()
     }
   }
   // вычитываем всё, что есть в serial в буфер
-  while (0 < (avail = Serial.available()) && serPos < sizeof(serBuf))
+  while (0 < (avail = Serial.available()) && 0 < sizeof(serBuf) - serPos)
   {
     size_t rest = sizeof(serBuf) - serPos;
     toRead = (avail > rest) ? rest : avail;
@@ -563,9 +383,38 @@ void TransparentIo()
   //BUILTIN_LED_OFF();
 }
 //-----------------------------------------------------------------------------
-static uint32_t connectCheckPeriod = 300;
-static uint32_t connectCheckLast = 0;
-
+void CheckTcpConnect()
+{
+  if ((millis() - connectCheckLast < connectCheckPeriod))
+    return;
+  connectCheckLast = millis();
+  if (WIFI_STA == WiFi.getMode() && WL_CONNECTED != WiFi.status())
+  {
+    DPPRINTLN("WIFI_STA disconected - do RESET");
+    WifiConnectReset();
+    return;
+  }
+  WiFiClient newClient = server.accept();
+  if (newClient)
+  {
+    if (tcpClient)
+      tcpClient.stop();
+    tcpClient = newClient;
+    // Configure keep-alive with custom parameters
+    // idle_sec: time before sending first keep-alive (default 7200 seconds)
+    // intv_sec: interval between keep-alive probes (default 75 seconds)
+    // count: number of probes before connection is considered broken (default 9)
+    // tcpClient.disableKeepAlive();
+    tcpClient.keepAlive(5, 3, 3);
+    tcpClient.setNoDelay(true);
+    //tcpClient.setTimeout(Server_Timeout);// default 1000
+    DPPRINT(F("Connected socket client: "));
+    DPPRINTLN(tcpClient.localIP());
+    DPPRINT(F("condition = ")); DPPRINTLN(wifiCondition, DEC);
+    ClearSerial();
+  }
+}
+//-----------------------------------------------------------------------------
 void WifiConnect()
 {
   switch (wifiCondition)
@@ -574,78 +423,23 @@ void WifiConnect()
       DPPRINTLN("unknown condition RESET");
       WifiConnectReset();
       break;
-
     case 0: // пробуем подключиться к точке доступа
       WifiConnectSTA();
       break;
-
     case 2: // стартуем точку доступа
       WifiConnectAP();
       break;
-
     case 10: // Ожидание подключения и обработка
-      if ((millis() - connectCheckLast > connectCheckPeriod))
-      {
-        connectCheckLast = millis();
-
-        if (WIFI_STA == WiFi.getMode() && WL_CONNECTED != WiFi.status())
-        {
-          DPPRINTLN("WIFI_STA disconected - do RESET");
-          WifiConnectReset();
-          return;
-        }
-
-        WiFiClient newClient = server.accept();
-        if (newClient)
-        {
-          if (tcpClient)
-            tcpClient.stop();
-          tcpClient = newClient;
-          // Configure keep-alive with custom parameters
-          // idle_sec: time before sending first keep-alive (default 7200 seconds)
-          // intv_sec: interval between keep-alive probes (default 75 seconds)
-          // count: number of probes before connection is considered broken (default 9)
-          // tcpClient.disableKeepAlive();
-          tcpClient.keepAlive(5, 3, 3);
-          tcpClient.setNoDelay(true);
-          //tcpClient.setTimeout(Server_Timeout);// default 1000
-          //wifi_set_sleep_type(NONE_SLEEP_T);
-          // wifi_set_sleep_type(MODEM_SLEEP_T);
-          // wifi_set_sleep_type(LIGHT_SLEEP_T);
-          DPPRINT(F("Connected socket client: "));
-          DPPRINTLN(tcpClient.localIP());
-          DPPRINT(F("condition = ")); DPPRINTLN(wifiCondition, DEC);
-          ClearSerial();
-        }
-      }
-      //ModbusIo();
+      CheckTcpConnect();
       TransparentIo();
       break; // case 10
   } // switch (wifiCondition)
 } // void WifiConnect()
 //----------------------------------------------------------------------
-void idle_blink()
-{
-  long unsigned int blinkPeriod = 1500;
-  switch (lastSource)
-  {
-    default: break;
-    case SOURCE_TCP:
-    case SOURCE_UDP:
-      BUILTIN_LED_ON(); return;
-      //blinkPeriod = 200; break;
-  }
-  if (millis() - gPreviosCycleQty > blinkPeriod)
-  {
-    BUILTIN_LED_TOOGLE();
-    gPreviosCycleQty = millis();
-  }
-}
-//----------------------------------------------------------------------
 void loop()
 {
   ESP.wdtFeed();
   WifiConnect();
-  idle_blink();
+  StatusBlink();
   //yield();
 }
